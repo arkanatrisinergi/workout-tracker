a27e487514162535066ca82b51660a7f14eda78c
arkanatrisinergi <arkanatrisinergi@gmail.com>
2026-05-19 11:31:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
     // ⚠️ PASTE YOUR COPIED GOOGLE APPS SCRIPT WEB APP URL HERE BETWEEN THE QUOTES
-    API_URL: "https://script.google.com/macros/s/AKfycbyefQ-PKNFhz11xvkREH26Ru5-TPbJyNnQvNTnFX7-51p_HYaQyJQ_YcmX6iJyIJyrstw/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbxLucCly1z9UNGX8VL7i7F6r8wCJHTB_eDk6AsnJ1x8eT4fuUuCznJwh8ZcnwvFLv1C/exec",
     VERSION: "1.2.0"
 };
